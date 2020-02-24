364c7427a2dadba207d82116391cfc325045760f
roguebones <45408853+roguebones@users.noreply.github.com>
2020-02-23 22:27:59 -0800

    Added functionality to take a single user shred photos url

--- scraper.py
+++ scraper.py
@@ -5,15 +5,40 @@ import urllib.request
 import urllib.parse
 import urllib
 import re
+import requests
 
 
-# use this image scraper from the location that
-# you want to save scraped images to
+# use this image scraper from the location that you want to save scraped images to
+
+def main():
+    #user_url = 'https://www.mountainproject.com/user/15116/blitzo/photos'
+    user_url = 'https://www.mountainproject.com/user/200084808/chase-g/photos'
+    download_all_images(user_url)
+
+    # TODO: take user input of user_url
 
 def make_soup(url):
     html = urllib.request.urlopen(url).read()
     return BeautifulSoup(html, features='html.parser')
 
+def make_url_list(url):
+
+    page_num = 1
+    url += '?page='
+    url_list = []
+
+    while True:
+        
+        page_url = url + str(page_num)
+        r = requests.get(page_url)
+        pat = re.search('https://www.mountainproject.com/photo/', r.text)
+        if pat is None:
+            break
+        else:
+            url_list.append(page_url)
+        page_num += 1
+
+    return url_list
 
 def get_images(url):
     soup = make_soup(url)
@@ -39,6 +64,15 @@ def get_images(url):
         urllib.request.urlretrieve(each, filename)
 
     return image_links
+
+def download_all_images(url):
+    
+    for item in make_url_list(url):
+        get_images(item)
+
+    # TODO: multithreading
+
+main()
 
 
 
